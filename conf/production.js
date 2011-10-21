367d2dfa1e297aa0fd3a19cb83aa79ce6801c3c4
cronopio <daristizabal@kumokojo.com>
2011-10-21 14:21:38 -0500

    [deploy] afinando el despliegue en nodejitsu

--- conf/production.js
+++ conf/production.js
@@ -5,16 +5,18 @@
 module.exports = function(app,express) {
 
   // Database connection
-  app.set('db-uri', 'mongodb://localhost/calipso-prod');
+  //app.set('db-uri', 'mongodb://localhost/calipso-prod');
+  // Testing in nodejitsu
+  app.set('db-uri', 'mongodb://nodejitsu:41059a5c57e2f5b800a4ab9ae36123e5@staff.mongohq.com:10038/nodejitsudb887361138599');
 
   // Change to suit - this key works for calip.so
-  app.set('google-analytics-key', 'UA-17607570-4');
+  //app.set('google-analytics-key', 'UA-17607570-4');
 
   // Disqus
   app.set('disqus-shortname', 'calipsojs');
 
   // App config
-  app.set('server-url', 'http://calip.so');
+  app.set('server-url', 'http://mercabits.nodejitsu.com');
 
 // Language mode
   app.set('language-add', false);
